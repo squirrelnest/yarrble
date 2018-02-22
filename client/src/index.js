7fa8483f6c63c5dc23f096d5d2b5255e428e931a
mikka <mikkanthrope@gmail.com>
2018-02-22 12:25:24 -0800

    Add NavBar component and body css

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './css/index.css';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
-//
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
-//
-// -----
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
