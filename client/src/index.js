13826989537695b32eccf096a9d419a7d4776be4
mikka <mikkanthrope@gmail.com>
2018-02-25 10:37:42 -0800

    no reducers, for real

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import changeCount from './reducers/changeCount';
-import createStore from './createStore';
-
-const store = createStore(changeCount);
 
 export function render() {
   ReactDOM.render(
-    <App store={store} />,
+    <App />,
     document.getElementById('root')
   );
 };
 
-store.dispatch({ type: '@@INIT' });
+render();
 
 // remove render() here
