151b761200b8e136f737523bb472cdd158b9ed1a
mikka <mikkanthrope@gmail.com>
2018-05-24 22:28:26 -0700

    Add authReducer

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,29 @@
+import {
+  AUTHENTICATED,
+  UNAUTHENTICATED,
+  AUTHENTICATION_ERROR,
+  ADD_USER
+} from '../actions/authActions'
+
+const initialState = {
+  user: []
+}
+
+/* Object.assign(target, source object, source object) copies values from one or more source objects to the target object then returns the target object */
+
+export default function authReducer(state = initialState, action) {
+  switch (action.type) {
+
+    case AUTHENTICATED:
+      return { ...state, authenticated: true }
+
+    case UNAUTHENTICATED:
+      return { ...state, authenticated: false }
+
+    case AUTHENTICATION_ERROR:
+      return { ...state, error: action.payload }
+
+    default:
+      return state
+  }
+}
