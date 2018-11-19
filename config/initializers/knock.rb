1ead71e4599171674e2bb112ef61b0edb1776584
mikka <mikkanthrope@gmail.com>
2018-11-18 23:05:06 -0800

    remove duplicate default config

--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -35,9 +35,9 @@ Knock.setup do |config|
   ##
   ## Configure the key used to sign tokens.
   ##
-  ## Default:
-  # config.token_secret_signature_key = -> { Rails.application.secrets.secret_key_base }
-  config.token_secret_signature_key = -> { Rails.application.credentials.fetch(:secret_key_base) }
+  # Default:
+  config.token_secret_signature_key = -> { Rails.application.secrets.secret_key_base }
+
   ## If using Auth0, uncomment the line below
   # config.token_secret_signature_key = -> { JWT.base64url_decode Rails.application.secrets.auth0_client_secret }
 
