3e823fb84db351665b3389a17d1d3ae35e90648e
mikka <mikkanthrope@gmail.com>
2019-01-29 22:11:36 -0800

    move api host to api.yarrble.com subdomain

--- client/src/api-config.js
+++ client/src/api-config.js
@@ -3,7 +3,7 @@ let backendHost;
 const hostname = window && window.location && window.location.hostname;
 
 if(hostname === 'yarrble.com') {
-  backendHost = 'yarrble.com:3001';
+  backendHost = 'api.yarrble.com:3001';
 } else if(hostname === 'localhost') {
   backendHost = 'localhost:3001';
 }
