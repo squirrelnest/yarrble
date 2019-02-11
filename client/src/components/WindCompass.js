46e20556b148a30401b0aba8ac47a63d890e38d1
mikka <mikkanthrope@gmail.com>
2019-02-11 12:52:41 -0800

    chain options for loc.winds

--- client/src/components/WindCompass.js
+++ client/src/components/WindCompass.js
@@ -15,37 +15,37 @@ export default class WindCompass extends Component {
       <div className={classes.dial}>
         <div className="row">
           <div className={classes.arrowContainer}>
-            <Arrow id="NW" interactive={false} css={[classes.arrowNorthwest, this.props.winds[0].NW ? classes.selected : null].join(' ')} />
+            <Arrow id="NW" css={[classes.arrowNorthwest, this.props.winds[0] && this.props.winds[0].NW && classes.selected].join(' ')} />
           </div>
           <div className={classes.arrowContainer}>
-            <Arrow id="N" interactive={false} css={[classes.arrowNorth, this.props.winds[0].N ? classes.selected : null].join(' ')}> 
+            <Arrow id="N" css={[classes.arrowNorth, this.props.winds[0] && this.props.winds[0].N && classes.selected].join(' ')}> 
               <div className={classes.arrowText}>N</div> 
             </Arrow> 
           </div>
           <div className={classes.arrowContainer}>
-            <Arrow id="NE" interactive={false} css={[classes.arrowNortheast, this.props.winds[0].NE ? classes.selected : null].join(' ')} />
+            <Arrow id="NE" css={[classes.arrowNortheast, this.props.winds[0] && this.props.winds[0].NE && classes.selected].join(' ')} />
           </div>
         </div>
         <div className="row">
           <div className={classes.arrowContainer}>
-            <Arrow id="W" interactive={false} css={[classes.arrowWest, this.props.winds[0].W ? classes.selected : null].join(' ')} />
+            <Arrow id="W" css={[classes.arrowWest, this.props.winds[0] && this.props.winds[0].W && classes.selected].join(' ')} />
           </div>
           <div className={classes.arrowContainer}>
             <Arrow className={classes.arrowBlank} />
           </div>
           <div className={classes.arrowContainer}>
-            <Arrow id="E" interactive={false} css={[classes.arrowEast, this.props.winds[0].E ? classes.selected : null].join(' ')} />
+            <Arrow id="E" css={[classes.arrowEast, this.props.winds[0] && this.props.winds[0].E && classes.selected].join(' ')} />
           </div>
         </div>
         <div className="row">
           <div className={classes.arrowContainer}>
-            <Arrow id="SW" interactive={false} css={[classes.arrowSouthwest, this.props.winds[0].SW ? classes.selected : null].join(' ')} />
+            <Arrow id="SW" css={[classes.arrowSouthwest, this.props.winds[0] && this.props.winds[0].SW && classes.selected].join(' ')} />
           </div>
           <div className={classes.arrowContainer}>
-            <Arrow id="S" interactive={false} css={[classes.arrowSouth, this.props.winds[0].S ? classes.selected : null].join(' ')} />
+            <Arrow id="S" css={[classes.arrowSouth, this.props.winds[0] && this.props.winds[0].S && classes.selected].join(' ')} />
           </div>
           <div className={classes.arrowContainer}>
-            <Arrow id="SE" interactive={false} css={[classes.arrowSoutheast, this.props.winds[0].SE ? classes.selected : null].join(' ')} />
+            <Arrow id="SE" css={[classes.arrowSoutheast, this.props.winds[0] && this.props.winds[0].SE && classes.selected].join(' ')} />
           </div>
         </div> 
       </div>
