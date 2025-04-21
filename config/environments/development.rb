84ef898496b6a777e496d1e2b53bdb382f761d6d
mikka <surfista@Mikkas-Laptop.local>
2025-04-20 21:32:04 -1000

    update backend dependencies

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,5 +43,8 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Use a simple file watcher to detect changes in source code
+  config.file_watcher = ActiveSupport::FileUpdateChecker
 end
