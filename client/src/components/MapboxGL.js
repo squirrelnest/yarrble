ec9fadc581d7ffc471967159cc4827e6bb159320
mikka <surfista@Mikkas-Laptop.local>
2025-04-26 00:11:51 -1000

    update map to use react-map-gl

--- client/src/components/MapboxGL.js
+++ client/src/components/MapboxGL.js
@@ -1,81 +1,46 @@
-import React, { Component } from "react";
-import mapboxgl from "mapbox-gl";
 import PlaceIcon from '@mui/icons-material/Place';
-import { Link } from 'react-router-dom';
-
-// jest.mock("mapbox-gl", () => { return {} });
+import * as React from 'react';
+import Map, { Marker } from 'react-map-gl/mapbox';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 const MAPBOX_TOKEN = "pk.eyJ1IjoiemVya29uaXVtIiwiYSI6ImNqZDE3MGFncDJtNjUyeG5zZGMwczMxcmEifQ.e0Pxb8cdU5NiEobS_o6zSg";
 
-export default class MapboxGL extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lng: 5,
-      lat: 34,
-      zoom: 1.5
-    };
-  }
+export default function MapboxGL(props) {
+  let markers = props.locations.map((loc) => 
+    <Marker
+      longitude={loc.lon}
+      latitude={loc.lat}
+      anchor="bottom"
+      onClick={() => window.location.href = `/locations/${loc.id}` }
+    >
+      <PlaceIcon
+        hoverColor={'#E91E63'}
+        color='#00BCD4'
+      />
+    </Marker>
+  )
 
-  componentDidUpdate(prevProps) {
-    if (this.state.map && (this.props.locations && this.props.locations !== prevProps.locations)) {
-      this.renderMarkers(this.props.locations, this.state.map)
-    }
-  }
-
-  componentDidMount() {
-    const { lng, lat } = this.state;
-
-    mapboxgl.accessToken = MAPBOX_TOKEN;
-
-    const map = new mapboxgl.Map({
-      container: this.mapContainer,
-      style: "mapbox://styles/mapbox/light-v9",
-      center: [lng, lat]
-    });
-
-    this.setState({map: map})
-    this.renderMarkers((this.props.locations || []), map)
-    
-    map.on("move", () => {
-      const { lng, lat } = map.getCenter();
-
-      this.setState({
-        lng: lng.toFixed(4),
-        lat: lat.toFixed(4),
-        zoom: map.getZoom().toFixed(2)
-      });
-    });
-  }
-
-  renderMarkers = (locations, map) => {
-    locations.forEach((loc) => {
-      // create a HTML element for each feature
-      let el = <Link
-        to={ `/locations/${loc.id}` }
-        key={ loc.id }>
-          <PlaceIcon
-            hoverColor={'#E91E63'}
-            color='#00BCD4'
-          />
-      </Link>;
-
-      // make a marker for each feature and add to the map
-      new mapboxgl.Marker(el)
-      .setLngLat([loc.lon, loc.lat])
-      .addTo(map);
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <div
-          style={{width: this.props.width, height:this.props.height }}
-          ref={el => (this.mapContainer = el)}
-          onClick={(event) => this.props.handleMapClick(event, event.lngLat)}
+  return (
+    <Map
+      mapboxAccessToken={MAPBOX_TOKEN}
+      initialViewState={{
+        longitude: 5,
+        latitude: 34,
+        zoom: 1.5
+      }}
+      mapStyle="mapbox://styles/mapbox/light-v9"
+      style={{
+        width: window.innerWidth < 700 ? window.innerWidth : window.innerWidth/2,
+        height: window.innerHeight,
+      }}
+    >
+      <Marker longitude={100} latitude={40} anchor="bottom" >
+        <PlaceIcon
+          hoverColor={'#E91E63'}
+          color='#00BCD4'
         />
-      </div>
-    );
-  }
+      </Marker>
+      { markers }
+    </Map>
+  )
 }
